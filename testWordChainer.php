1215fbc3d8c5f811e1d0dd8aab42edeeb589623c
Dotonomic <github@dotonomic.com>
2023-12-05 16:03:33 +0000

    Add files via upload

---
+++ testWordChainer.php
@@ -0,0 +1,26 @@
+<?php
+
+require 'WordChainer.php';
+
+// https://github.com/openai-php/client
+require 'vendor/autoload.php';
+$LLMclient = OpenAI::client(/* OpenAI API key */);
+$LLM = 'gpt-4-1106-preview';
+
+$WordChainer = new WordChainer($LLMclient,$LLM);
+
+$firstWord = 'anger';
+$lastWord = 'ball';
+$chainLength = 7;
+$tries = 5;
+$scoredChains = $WordChainer->scoredChains($firstWord,$lastWord,$chainLength,$tries);
+
+$chains = $scoredChains[0];
+$scores = $scoredChains[1];
+
+for ($i=1; $i<=$tries; $i++) {
+	echo $chains[$i-1].'<br>';
+	echo preg_replace('/]/',']<br>',$scores[$i-1]).'<br><br>';
+}
+
+?>
